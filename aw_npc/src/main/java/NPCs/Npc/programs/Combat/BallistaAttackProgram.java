0d78dd06e442a55b41066ecfa7a6756e64e02999
Marvin Eckhardt <85019164+dercodeKoenig@users.noreply.github.com>
2025-03-05 21:13:53 +0100

    .

--- aw_npc/src/main/java/NPCs/Npc/programs/Combat/BallistaAttackProgram.java
+++ aw_npc/src/main/java/NPCs/Npc/programs/Combat/BallistaAttackProgram.java
@@ -50,7 +50,7 @@ public class BallistaAttackProgram extends Goal {
 
     public boolean isPositionLocked(BlockPos p) {
         // if I lock the position, it is not locked for ME, only for OTHER WORKERS
-        if (ballista !=null && Objects.equals(p, ballista.blockPosition())) return false;
+        if (ballista != null && Objects.equals(p, ballista.blockPosition())) return false;
 
         long gameTime = npc.level().getGameTime();
         return (positionsInUseWithLastUseTime.containsKey(p) &&
@@ -75,26 +75,42 @@ public class BallistaAttackProgram extends Goal {
         }
         lastCheck = npc.level().getGameTime();
 
-        boolean c = npc.getTarget() != null && npc.getTarget().isAlive() && npc.hunger > npc.maxHunger * 0.05&& npc.position().distanceTo(npc.getTarget().position()) > 6;
+        boolean c = npc.getTarget() != null && npc.getTarget().isAlive() && npc.hunger > npc.maxHunger * 0.05 && npc.position().distanceTo(npc.getTarget().position()) > 6;
         if (!c) return false;
 
         List<Ballista> nearbyBallistas = npc.level().getEntitiesOfClass(Ballista.class, new AABB(npc.blockPosition()).inflate(128));
-        for (Ballista i : sortedEntitiesByDistanceTo(nearbyBallistas,npc.position())) {
+        for (Ballista i : sortedEntitiesByDistanceTo(nearbyBallistas, npc.position())) {
             // do not work this ballista when other hostile creatures are around. consider it a enemy ballista
-           List<LivingEntity> entitiesAroundBallista = npc.level().getEntitiesOfClass(LivingEntity.class, new AABB(i.blockPosition()).inflate(8));
-           boolean canUse = true;
-           for (LivingEntity j : entitiesAroundBallista){
-               if(HostileEntities.shouldAttack(j,npc)){
-                   canUse = false;
-                   break;
-               }
-           }
+            List<LivingEntity> entitiesAroundBallista = npc.level().getEntitiesOfClass(LivingEntity.class, new AABB(i.blockPosition()).inflate(8));
+            for (LivingEntity j : entitiesAroundBallista) {
+                if (HostileEntities.shouldAttack(j, npc)) {
+                    //System.out.println(i+":tc");
+                    return false;
+                }
+            }
+
+            if (i.bolt == null) return false;
+
+            List<Entity> entities = npc.level().getEntities((Entity) null, i.getBoundingBox().expandTowards(npc.getTarget().position().subtract(i.position())).inflate(1), (Predicate<Entity>) entity -> true);
+            // scan for friendly entities in area
+            for (Entity entity1 : entities) {
+                AABB aabb = entity1.getBoundingBox().inflate(1);
+                Optional<Vec3> optional = aabb.clip(i.bolt.position(), i.bolt.position().add(npc.getTarget().position().subtract(i.bolt.position()).normalize().scale(100)));
+                if (optional.isPresent()) {
+                    if(entity1 != i) {
+                        if (HostileEntities.isUnableToAttack(entity1, npc)) {
+                            //System.out.println(i+":ff");
+                            return false;
+                        }
+                    }
+                }
+            }
 
             Vec3 vec3 = new Vec3(i.getX(), i.getEyeY(), i.getZ());
             Vec3 vec31 = new Vec3(npc.getTarget().getX(), npc.getTarget().getEyeY(), npc.getTarget().getZ());
-            boolean  canSee= npc.level().clip(new ClipContext(vec3, vec31, ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, npc)).getType() == HitResult.Type.MISS;
+            boolean canSee = npc.level().clip(new ClipContext(vec3, vec31, ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, npc)).getType() == HitResult.Type.MISS;
 
-            if (canSee && canUse && (i.controllingEntity == null || Objects.equals(i.controllingEntity,npc.getUUID())) && isPositionWorkable(i.blockPosition()) && i.bolt != null && i.getEntityData().get(Ballista.CONSTRUCTION_PROGRESS) == 17 && !i.getEntityData().get(Ballista.IS_BROKEN)) {
+            if (canSee && (i.controllingEntity == null || Objects.equals(i.controllingEntity, npc.getUUID())) && isPositionWorkable(i.blockPosition())  && i.getEntityData().get(Ballista.CONSTRUCTION_PROGRESS) == 17 && !i.getEntityData().get(Ballista.IS_BROKEN)) {
                 ballista = i;
                 lockTargetPosition();
                 return true;
@@ -104,7 +120,7 @@ public class BallistaAttackProgram extends Goal {
     }
 
     public boolean canContinueToUse() {
-        return ballista != null && npc.getTarget() != null && npc.getTarget().isAlive() && npc.hunger > npc.maxHunger * 0.05&& npc.position().distanceTo(npc.getTarget().position()) > 6;
+        return ballista != null && npc.getTarget() != null && npc.getTarget().isAlive() && npc.hunger > npc.maxHunger * 0.05 && npc.position().distanceTo(npc.getTarget().position()) > 6;
     }
 
     public void start() {
@@ -134,13 +150,14 @@ public class BallistaAttackProgram extends Goal {
         ballista.controllingEntity = npc.getUUID();
 
         double distToTarget;
-            int exit = npc.slowMobNavigation.moveToPosition(ballista.blockPosition(), 1, npc.slowNavigationMaxDistance, npc.slowNavigationMaxNodes, npc.slowNavigationStepPerTick, (float) speedModifier);
-            if (exit == SUCCESS_STILL_RUNNING)
-                return;
-            if (exit == EXIT_FAIL) {
-                ballista = null;
-                return;
-            }
+        int exit = npc.slowMobNavigation.moveToPosition(ballista.blockPosition(), 2, npc.slowNavigationMaxDistance, npc.slowNavigationMaxNodes, npc.slowNavigationStepPerTick, (float) speedModifier);
+        if (exit == SUCCESS_STILL_RUNNING)
+            return;
+        if (exit == EXIT_FAIL) {
+            //System.out.println("failed to move to "+ballista);
+            ballista = null;
+            return;
+        }
 
         LivingEntity livingentity = this.npc.getTarget();
         if (livingentity != null) {
@@ -149,7 +166,7 @@ public class BallistaAttackProgram extends Goal {
             Vec3 targetPosition = ballista.position().subtract(lookNoY.normalize().scale(2.0)).add(new Vec3((double) 0.0F, (double) 0.5F, (double) 0.0F));
             distToTarget = npc.position().distanceTo(targetPosition);
             if (distToTarget > 1) {
-                if (npc.slowMobNavigation.pathFinder.findPath(new BlockPos(Mth.floor(targetPosition.x),Mth.floor(targetPosition.y),Mth.floor(targetPosition.z)), 5, 0, 10, 1000).exitCode == EXIT_SUCCESS) {
+                if (npc.slowMobNavigation.pathFinder.findPath(new BlockPos(Mth.floor(targetPosition.x), Mth.floor(targetPosition.y), Mth.floor(targetPosition.z)), 5, 0, 10, 1000).exitCode == EXIT_SUCCESS) {
                     npc.getMoveControl().setWantedPosition(targetPosition.x, targetPosition.y, targetPosition.z, 1);
                     return;
                 }
@@ -165,7 +182,7 @@ public class BallistaAttackProgram extends Goal {
                 double d1 = livingentity.getZ() - ballista.bolt.getZ();
                 double targetYRot = (Mth.atan2(d1, d0) * (double) 180.0F / (double) (float) Math.PI) - 90.0F;
                 ballista.targetYRot = (float) targetYRot;
-                if (Math.abs((360+ballista.getYRot()) % 360 - (360 + ballista.targetYRot) % 360) < 0.1) {
+                if (Math.abs((360 + ballista.getYRot()) % 360 - (360 + ballista.targetYRot) % 360) < 0.1) {
                     double d2 = livingentity.getY(0.5) - (ballista.bolt.getY());
                     double d3 = Math.sqrt(d0 * d0 + d1 * d1); // Horizontal distance
 
@@ -186,9 +203,11 @@ public class BallistaAttackProgram extends Goal {
                             AABB aabb = entity1.getBoundingBox().inflate(1);
                             Optional<Vec3> optional = aabb.clip(ballista.bolt.position(), ballista.bolt.position().add(ballista.getLookAngle().normalize().scale(100)));
                             if (optional.isPresent()) {
-                                if (HostileEntities.isUnableToAttack(entity1, npc)) {
-                                    freeToFire = false;
-                                    break;
+                                if(entity1 != ballista) {
+                                    if (HostileEntities.isUnableToAttack(entity1, npc)) {
+                                        freeToFire = false;
+                                        break;
+                                    }
                                 }
                             }
                         }
