4c059bbf4c08ce9db647c4842c227d0b03632931
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2025-10-10 20:07:27 +0200

    Update EntityRocketAssembler.java

--- advRocketry/src/main/java/advRocketry/BlockEntities/EntityRocketAssembler.java
+++ advRocketry/src/main/java/advRocketry/BlockEntities/EntityRocketAssembler.java
@@ -102,8 +102,8 @@ public class EntityRocketAssembler extends BlockEntity implements ARLib.network.
 
                     boolean isAllValid = true;
                     // see if for the current rectangle all positions are a valid launchpad
-                    for (int x = minPos.getX(); x < maxPos.getX(); x++) {
-                        for (int z = minPos.getZ(); z < maxPos.getZ(); z++) {
+                    for (int x = minPos.getX(); x <= maxPos.getX(); x++) {
+                        for (int z = minPos.getZ(); z <= maxPos.getZ(); z++) {
                             BlockPos target = new BlockPos(x, startingPos.getY(), z);
                             Block block = level.getBlockState(target).getBlock();
                             if (!(block instanceof LaunchPad)) {
@@ -162,6 +162,7 @@ public class EntityRocketAssembler extends BlockEntity implements ARLib.network.
     }
 
     public void scanArea() {
+        long t0 = System.currentTimeMillis();
         Dimension myDim = DimensionManager.get(level.dimension().location());
         if (myDim != null && myDim.getType() == DimensionProperties.PlanetType.SPACE_STATION) {
             scanForSpaceDockingArea();
@@ -169,6 +170,8 @@ public class EntityRocketAssembler extends BlockEntity implements ARLib.network.
             scanForLaunchPadArea();
         }
         broadcastInformationToPlayers(null);
+        long t1 = System.currentTimeMillis();
+        System.out.println("scan complete in " +(t1-t0) +"ms");
 
         if(areaMin != null)level.setBlock(areaMin,Blocks.DIAMOND_BLOCK.defaultBlockState(), 3);
         if(areaMax != null)level.setBlock(areaMax,Blocks.DIAMOND_BLOCK.defaultBlockState(), 3);
