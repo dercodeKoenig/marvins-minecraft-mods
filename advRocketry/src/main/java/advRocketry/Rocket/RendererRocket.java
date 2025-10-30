11d3022e7f13dd212699f02ba8327bd935f267b9
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2025-10-30 02:48:30 +0100

    ,.

--- advRocketry/src/main/java/advRocketry/Rocket/RendererRocket.java
+++ advRocketry/src/main/java/advRocketry/Rocket/RendererRocket.java
@@ -17,8 +17,10 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.client.model.data.ModelData;
 import org.joml.Quaternionf;
+import org.joml.Vector3f;
 
 import java.util.Random;
 
@@ -40,20 +42,19 @@ public class RendererRocket extends EntityRenderer<EntityRocket> {
         poseStack.pushPose();
         poseStack.translate(-(float) p_entity.size.getX() / 2, 0, -(float) p_entity.size.getZ() / 2);
 
-        poseStack.rotateAround(
-                Axis.YP.rotationDegrees(p_entity.getYRot()),
-                (float)p_entity.size.getX() / 2,  // pivot x
-                0,                       // pivot y
-                (float)p_entity.size.getZ() / 2   // pivot z
-        );
+        Vec3 headingNormal = p_entity.heading.cross(new Vec3(0,1,0));
+        double angleDeg = -Math.acos(p_entity.heading.dot(new Vec3(0,1,0))) *180 / Math.PI;
 
-        poseStack.rotateAround(
-                Axis.XP.rotationDegrees(p_entity.getXRot()),
-                (float)p_entity.size.getX() / 2,  // pivot x
-                0,                       // pivot y
-                (float)p_entity.size.getZ() / 2   // pivot z
-        );
-
+        if (headingNormal.length() > 0.000001){
+            poseStack.rotateAround(
+                    new Quaternionf().fromAxisAngleDeg(headingNormal.toVector3f(),(float)angleDeg),
+                    (float)p_entity.size.getX() / 2,  // pivot x
+                    p_entity.size.getY() / 2,                       // pivot y
+                    (float)p_entity.size.getZ() / 2   // pivot z
+            );
+        }else{
+            // 180°? well you are fucked now
+        }
 
         for (BlockPos p : p_entity.blocks.keySet()) {
             BlockState state = p_entity.blocks.get(p);
--- advRocketry/src/main/java/advRocketry/mixins/EntityMixin.java
+++ advRocketry/src/main/java/advRocketry/mixins/EntityMixin.java
@@ -2,28 +2,33 @@ package advRocketry.mixins;
 
 import advRocketry.Dimension.Dimension;
 import advRocketry.Dimension.DimensionManager;
-import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.entity.item.ItemEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin {
-    @Inject(method = "getDefaultGravity", at = @At("HEAD"), cancellable = true)
-    public void getDefaultGravity(CallbackInfoReturnable<Double> cir) {
-        LivingEntity entity = (LivingEntity) (Object) this;
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+    @Inject(method = "applyGravity", at = @At("HEAD"), cancellable = true)
+    public void applyGravity(CallbackInfo ci) {
+        Entity entity = (Entity) (Object) this;
         ResourceLocation dimensionId = entity.level().dimension().location();
         Dimension dimension = DimensionManager.get(dimensionId);
+        double massMultiplier = 1;
         if (dimension != null) { // registered in DimensionManager
-            double massMultiplier = dimension.getEarthMassMultiplier();
-           cir.setReturnValue((entity.getAttributeValue(Attributes.GRAVITY) * massMultiplier));
+            massMultiplier = dimension.getEarthMassMultiplier();
         }
+
+        double d0 = entity.getGravity() * massMultiplier;
+        if (d0 != (double)0.0F) {
+            entity.setDeltaMovement(entity.getDeltaMovement().add((double)0.0F, -d0, (double)0.0F));
+        }
+        ci.cancel();
     }
 }
