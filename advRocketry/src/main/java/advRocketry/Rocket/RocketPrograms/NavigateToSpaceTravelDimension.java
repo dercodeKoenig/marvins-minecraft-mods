6cc3bb8e084e22fd32ee237b6f8565d5a208a290
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2025-11-06 22:30:56 +0100

    Update NavigateToSpaceTravelDimension.java

--- advRocketry/src/main/java/advRocketry/Rocket/RocketPrograms/NavigateToSpaceTravelDimension.java
+++ advRocketry/src/main/java/advRocketry/Rocket/RocketPrograms/NavigateToSpaceTravelDimension.java
@@ -34,37 +34,42 @@ public class NavigateToSpaceTravelDimension {
             rocket.enableSecondaryEngines(false, false);
             rocket.setTargetPosition(new Vec3(rocket.position().x, Config.INSTANCE.planetSkyHeight + 500, rocket.position().z), false);
 
-            if (rocket.position().y > Config.INSTANCE.planetSkyHeight && rocket.level() instanceof ServerLevel serverLevel) {
-                // teleport to space travel dimension
+            if(rocket.position().y > Config.INSTANCE.planetSkyHeight) {
+                if (rocket.level() instanceof ServerLevel serverLevel) {
+                    // teleport to space travel dimension
 
-                if (myDim != null) {
-                    // for planets, move to the correct position relative to the planet.
-                    if(myDim instanceof PlanetDimension p){
-                        double r = CelestialUtils.toAU(p.getEarthRadiusMultiplier() * CelestialUtils.EARTH_RADIUS * Config.INSTANCE.planetRenderScaleMultiplier * 1.1 + Config.INSTANCE.planetSkyHeight * 10);
-                        Vec3 planetUp = p.getGlobalAxisDirections(0, p.getLatitudeFromZPosition(rocket.position().z)).up;
-                        rocket.universePosition = myDim.getPosition(0).add(planetUp.scale(r));
-                        rocket.universeHeading = planetUp.normalize();
-                        rocket.universeFront = rocket.universeHeading.cross(new Vec3(1, 0, 0));
-                        if (rocket.universeFront.length() < 0.01)
-                            rocket.universeFront = rocket.universeHeading.cross(new Vec3(0, 0, 1));
-                    }else{
-                        rocket.universePosition = myDim.getPosition(0);
+                    if (myDim != null) {
+                        // for planets, move to the correct position relative to the planet.
+                        if (myDim instanceof PlanetDimension p) {
+                            double r = CelestialUtils.toAU(p.getEarthRadiusMultiplier() * CelestialUtils.EARTH_RADIUS * Config.INSTANCE.planetRenderScaleMultiplier * 1.1 + Config.INSTANCE.planetSkyHeight * 10);
+                            Vec3 planetUp = p.getGlobalAxisDirections(0, p.getLatitudeFromZPosition(rocket.position().z)).up;
+                            rocket.universePosition = myDim.getPosition(0).add(planetUp.scale(r));
+                            rocket.universeHeading = planetUp.normalize();
+                            rocket.universeFront = rocket.universeHeading.cross(new Vec3(1, 0, 0));
+                            if (rocket.universeFront.length() < 0.01)
+                                rocket.universeFront = rocket.universeHeading.cross(new Vec3(0, 0, 1));
+                        } else {
+                            rocket.universePosition = myDim.getPosition(0);
+                        }
                     }
+
+                    // get the teleportation target
+                    ServerLevel target = DimensionManager.getServerLevel(serverLevel.getServer(), RocketTravelDimension.dimId);
+                    ChunkPos targetPos = RocketTravelDimension.getNextFreeChunkPos();
+                    BlockPos targetBlockPos = targetPos.getMiddleBlockPosition(100);
+
+
+                    rocket.teleportTo(target, targetBlockPos.getCenter(), new Vec3(0, 0, 0));
+
+
+                    // initial command to force load the chunk so that the rocket starts ticking
+                    RocketTravelDimension.keepChunkLoaded(targetPos);
+
+                    // newRocket.endProgram();
+                }else{
+                    // client side, while waiting on dimension transition do not stop and rotate the rocket midflight, just keep going and wait for teleport to kick in
+                    rocket.setTargetPosition(new Vec3(rocket.position().x, rocket.position().y + 500, rocket.position().z), false);
                 }
-
-                // get the teleportation target
-                ServerLevel target = DimensionManager.getServerLevel(serverLevel.getServer(), RocketTravelDimension.dimId);
-                ChunkPos targetPos = RocketTravelDimension.getNextFreeChunkPos();
-                BlockPos targetBlockPos = targetPos.getMiddleBlockPosition(100);
-
-
-                rocket.teleportTo(target, targetBlockPos.getCenter(), new Vec3(0,0,0));
-
-
-                // initial command to force load the chunk so that the rocket starts ticking
-                RocketTravelDimension.keepChunkLoaded(targetPos);
-
-                // newRocket.endProgram();
             }
         }
 
