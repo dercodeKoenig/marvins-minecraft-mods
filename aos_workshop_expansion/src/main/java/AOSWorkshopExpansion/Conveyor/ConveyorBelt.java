f14a0fa3c479196cf152cdf304eb447b94651014
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-02-20 02:10:06 +0100

    better placement

--- aos_workshop_expansion/src/main/java/AOSWorkshopExpansion/Conveyor/ConveyorBelt.java
+++ aos_workshop_expansion/src/main/java/AOSWorkshopExpansion/Conveyor/ConveyorBelt.java
@@ -13,6 +13,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -34,7 +35,7 @@ import java.util.List;
 
 import static AOSWorkshopExpansion.Registry.ENTITY_CONVEYOR_BELT;
 
-public class ConveyorBelt extends Block implements EntityBlock , ItemHammer.HammerInteractionBlock {
+public class ConveyorBelt extends Block implements EntityBlock, ItemHammer.HammerInteractionBlock {
 
     public static EnumProperty<Direction> FACING = BlockStateProperties.HORIZONTAL_FACING;
     public static BooleanProperty DIAGONAL = BooleanProperty.create("diagonal");
@@ -48,9 +49,9 @@ public class ConveyorBelt extends Block implements EntityBlock , ItemHammer.Hamm
 
     @Override
     public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
-        tooltipComponents.add(Component.literal("Max Stress: "+ ConveyorConfig.INSTANCE.conveyorMaxStress));
-        tooltipComponents.add(Component.literal("Friction: "+ConveyorConfig.INSTANCE.conveyorResistance));
-        tooltipComponents.add(Component.literal("Inertia: "+ConveyorConfig.INSTANCE.conveyorInertia));
+        tooltipComponents.add(Component.literal("Max Stress: " + ConveyorConfig.INSTANCE.conveyorMaxStress));
+        tooltipComponents.add(Component.literal("Friction: " + ConveyorConfig.INSTANCE.conveyorResistance));
+        tooltipComponents.add(Component.literal("Inertia: " + ConveyorConfig.INSTANCE.conveyorInertia));
         tooltipComponents.add(Component.literal("place above conveyor engine"));
     }
 
@@ -71,16 +72,43 @@ public class ConveyorBelt extends Block implements EntityBlock , ItemHammer.Hamm
         if (placer != null) {
             state = state.setValue(FACING, placer.getDirection());
             state = state.setValue(DIAGONAL, false);
+            state = updateFromNeighbourShapes(state, level, pos);
             level.setBlock(pos, state, 3);
         }
-        super.setPlacedBy(level, pos, state, placer, stack); // Call the super method for any additional behavior
+    }
+
+    @Override
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos pos, BlockPos neighborPos) {
+
+        if(direction == Direction.DOWN || direction == Direction.UP)
+            return state;
+
+        BlockEntity tile = level.getBlockEntity(pos);
+        BlockEntity other = level.getBlockEntity(neighborPos);
+        if (tile instanceof EntityConveyorBelt conveyorBelt){
+            if (!conveyorBelt.getConnectedParts(conveyorBelt, null).isEmpty() && direction.getAxis() != state.getValue(FACING).getAxis()) {
+                // can not rotate to a different direction if already connected
+                // but we still can see if we might need to change the diagonal
+                return state;
+            }
+        }
+
+        if(other instanceof EntityConveyorBelt otherBelt){
+            // if the neighbor aligns with the axis we rotate towards it
+            // if the neighbor has no connected parts, we rotate there even if it doesn't align because it will align in next tick
+            if(otherBelt.getConnectedParts(otherBelt,null).isEmpty() || neighborState.getValue(FACING).getAxis() == direction.getAxis()){
+                state = state.setValue(FACING, direction);
+            }
+        }
+
+        return state;
     }
 
     @Override
     public void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean isMoving) {
         BlockEntity blockEntity = level.getBlockEntity(pos);
         if (blockEntity instanceof EntityConveyorBelt conveyorBelt) {
-            for(Long id : new ArrayList<>(conveyorBelt.id_items.keySet())) {
+            for (Long id : new ArrayList<>(conveyorBelt.id_items.keySet())) {
                 conveyorBelt.popItem(id, Direction.UP);
             }
         }
@@ -104,11 +132,11 @@ public class ConveyorBelt extends Block implements EntityBlock , ItemHammer.Hamm
 
     @Override
     public InteractionResult onHammer(ItemStack itemStack, Level level, BlockPos blockPos, BlockState blockState, Player player, InteractionHand interactionHand) {
-        if(player == null)return InteractionResult.PASS;
+        if (player == null) return InteractionResult.PASS;
 
         BlockState block = level.getBlockState(blockPos);
-        if(block.getBlock() instanceof ConveyorBelt){
-            if(player.isShiftKeyDown())
+        if (block.getBlock() instanceof ConveyorBelt) {
+            if (player.isShiftKeyDown())
                 block = block.setValue(DIAGONAL, !block.getValue(DIAGONAL));
             else
                 block = block.setValue(FACING, block.getValue(FACING).getClockWise());
@@ -116,7 +144,7 @@ public class ConveyorBelt extends Block implements EntityBlock , ItemHammer.Hamm
 
             // after state change, make sure the BE updates its meshdata for render
             BlockEntity entity = level.getBlockEntity(blockPos);
-            if(entity instanceof EntityConveyorBelt entityConveyorBelt)
+            if (entity instanceof EntityConveyorBelt entityConveyorBelt)
                 entityConveyorBelt.requiresMeshUpdate = true;
         }
         return InteractionResult.SUCCESS;
