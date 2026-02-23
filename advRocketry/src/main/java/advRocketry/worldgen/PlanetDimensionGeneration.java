4836444b78158a66f0d67972d361856bdf0cae33
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-02-23 21:23:57 +0100

    Update PlanetDimensionGeneration.java

--- advRocketry/src/main/java/advRocketry/worldgen/PlanetDimensionGeneration.java
+++ advRocketry/src/main/java/advRocketry/worldgen/PlanetDimensionGeneration.java
@@ -54,7 +54,7 @@ public class PlanetDimensionGeneration {
         ChunkGenerator generator = new CustomChunkGenerator(
                 MultiNoiseBiomeSource.createFromList(new Climate.ParameterList<>(biomeConfig.createBiomeConfig())),
                 Holder.direct(new NoiseGeneratorSettings(
-                        new NoiseSettings(-64, 384, 2, 2),
+                        new NoiseSettings(-64, 384, 1, 2), // overworld settings
                         defaultBlock,
                         defaultFluid,
                         overworldSettings.noiseRouter(),
