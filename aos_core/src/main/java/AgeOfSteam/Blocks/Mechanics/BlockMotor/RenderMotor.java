3b1af77bae56f84fd81de1ddc03de422058cd44f
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-03-01 02:17:16 +0100

    todo: make render more robust

--- aos_core/src/main/java/AgeOfSteam/Blocks/Mechanics/BlockMotor/RenderMotor.java
+++ aos_core/src/main/java/AgeOfSteam/Blocks/Mechanics/BlockMotor/RenderMotor.java
@@ -47,7 +47,10 @@ public class RenderMotor implements BlockEntityRenderer<EntityMotor> {
     @Override
     public void render(EntityMotor tile, float partialTick, PoseStack stack, MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
 
-        if (tile.lastLight != packedLight) {
+        if(tile.vertexBuffer == null || tile.vertexBuffer.isInvalid())
+            return;
+
+        if (tile.lastLight != packedLight || tile.mesh == null) {
             tile.lastLight = packedLight;
             ByteBufferBuilder byteBuffer = new ByteBufferBuilder(1024);
             BufferBuilder b = new BufferBuilder(byteBuffer, VertexFormat.Mode.TRIANGLES, POSITION_COLOR_TEXTURE_NORMAL_LIGHT);
