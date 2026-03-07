3f3abf2b8db1f85f2ee9a999f61cfc5ba0e03e6b
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-03-07 15:16:19 +0100

    Create ItemSatellite.java

---
+++ advRocketry/src/main/java/advRocketry/Items/ItemSatellite.java
@@ -0,0 +1,28 @@
+package advRocketry.Items;
+
+import advRocketry.Satellites.Satellite;
+import advRocketry.Satellites.SatelliteRegistry;
+import advRocketry.utils.ItemUtils;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class ItemSatellite extends Item {
+    public ItemSatellite(Properties properties) {
+        super(new Properties().stacksTo(1));
+    }
+
+    public static Satellite createFromItemOrCreateNew(ItemStack stack, HolderLookup.Provider registries) {
+        CompoundTag tag = ItemUtils.getStacktagOrEmpty(stack);
+        Satellite satellite = SatelliteRegistry.createFromNbt(tag, registries);
+        if (satellite == null)
+            satellite = new Satellite();
+        return satellite;
+    }
+
+    public static void saveToStack(ItemStack stack, Satellite satellite, HolderLookup.Provider registries) {
+        CompoundTag tag = SatelliteRegistry.saveToNbt(satellite, registries);
+        ItemUtils.setTag(stack, tag);
+    }
+}
