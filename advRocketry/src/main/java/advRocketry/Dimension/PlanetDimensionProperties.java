bb01bf32b244e21198a4bf1be9a402cc0eca2f4d
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-03-12 00:30:04 +0100

    restructure property file

--- advRocketry/src/main/java/advRocketry/Dimension/PlanetDimensionProperties.java
+++ advRocketry/src/main/java/advRocketry/Dimension/PlanetDimensionProperties.java
@@ -4,6 +4,8 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.phys.Vec3;
 import org.joml.Vector3f;
 
+import java.util.HashMap;
+
 
 public class PlanetDimensionProperties extends DimensionProperties{
     // TODO: add always rain / always thunder values or custom rain times
@@ -16,65 +18,50 @@ public class PlanetDimensionProperties extends DimensionProperties{
         this.type = DimensionType.PLANET;
     }
 
+    // planet related configs
     public Vec3 position = new Vec3(0, 0, 0);
-
-    public boolean hasCustomSky = true;
-
     public float gravitationalMultiplier = 1;
-
     public float earthRadiusMultiplier = 1;
-
     public Vec3 rotationAxis = new Vec3(0.2, 1, 0);
-
-    public int seaLevel = 63; // to calculate wet level for terraforming use current sea level - original sea level (maybe) ?
-
-    public int originalSeaLevel = 63;
-
-    public boolean generateStructures = false;
-
     public ResourceLocation parentDimensionId = null;       // optional, overwrites position
-
     public Vec3 orbitAxis = new Vec3(0, 1, 0);
-
     public float orbitalDistanceToParent = 1;
-
     public float orbitalBaseOffsetDegrees = 0;
-
     public ResourceLocation dayTimeReference = null;  // required reference for day start
 
-    public ResourceLocation texture = null;
-
-    public Vector3f skyColor = new Vector3f(0.45f, 0.7f, 1f); // normal sky color
-
-    public Vector3f cloudColor = new Vector3f(1,1,1);
-
-    public Vector3f fogColor = new Vector3f(0.89f, 0.95f, 1.0f); // base fog color to calculate actual color
-
-    public Vector3f sunRiseColor = new Vector3f(3f, 2f, 0.2f); // the atm shading on sunrise
-
-    public Vector3f reflectiveTextureTintColor = new Vector3f(1f, 1f, 1f); // maybe reflect only green light? or red?
-
-    public Vector3f emissiveColor = new Vector3f(0, 0, 0); // the color that the planet radiates with for render
-
-    public boolean hasRingSystem = false;
-
-    public float radiationIntensity; // radiation strength, used for terrain shading, and temperature calculation and to scale emissive light in planet render
-
-    public float atmosphereDensity = 1; // TODO: calculate based on components, co2, o2, and other gases
-
     public int latitude_len = 400000;// how much you have to move in z direction to "go around the planet" 0% = equator, 25% = South Pole, 50% = equator again, 75% = North Pole
-
     public int targetDayLength = 24000; // set negative or 0 for fixed time
 
-    public float dayTime;
-
-    public boolean isKnown = false; // if false it has to be discovered and unlocked in observatory
-
     public boolean canVisit = false;
-
+    public boolean canGasMine = false;
+    public boolean isKnown = false; // if false it has to be discovered and unlocked in observatory
+    public int dataRequiredForUnlock = 2000; // how much data of any type is required to unlock it on the planet
     public ResourceLocation artifactItem = null; // TODO: artifact allows for discovery in observatory
 
+    // world gen related configs
+    public int seaLevel = 63; // to calculate wet level for terraforming use current sea level - original sea level (maybe) ?
+    public int targetSeaLevel = 63; // maybe save in chunk data when a chunk has fully adjusted sea level, use chunk load event to replace sea level instantly for new chunks?
+    public boolean generateStructures = false;
     public String biomePreset = null;
+    public boolean generateVolcanos = false;
 
-    public int dataRequiredForUnlock = 2000; // how much data of any type is required to unlock it on the planet
+
+    // mostly render related configs
+    public ResourceLocation texture = null;
+    public boolean hasCustomSky = true;
+    public float radiationIntensity = 0; // radiation strength, used for terrain shading, and temperature calculation and to scale emissive light in planet render
+    public Vector3f skyColor = new Vector3f(0.45f, 0.7f, 1f); // also used in atm shading when looking from a distant planet to this one
+    public Vector3f cloudColor = new Vector3f(1,1,1); // TODO: calculate based on atm when null
+    public Vector3f fogColor = new Vector3f(0.89f, 0.95f, 1.0f); // base fog color to calculate actual color
+    public Vector3f sunRiseColor = new Vector3f(3f, 2f, 0.2f); // the atm shading on sunrise
+    public Vector3f reflectiveTextureTintColor = new Vector3f(1f, 1f, 1f); // maybe reflect only green light? or red?
+    public Vector3f emissiveColor = new Vector3f(0, 0, 0); // the color that the planet radiates with for render
+    public boolean hasRingSystem = false;
+
+
+    public float atmosphereDensity = 1; // TODO: calculate based on components, co2, o2, and other gases
+    public HashMap<String, Double> atmosphereComposition = new HashMap<>(); // fluid id, relative value, sum of 1 -> atm density = 1
+
+
+    public float dayTime; // do not set yourself
 }
