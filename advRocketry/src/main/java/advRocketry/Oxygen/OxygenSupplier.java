91b1f1c69ff143954b51e64e5a0d12eee92fc541
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-03-14 20:17:06 +0100

    Update OxygenSupplier.java

--- advRocketry/src/main/java/advRocketry/Oxygen/OxygenSupplier.java
+++ advRocketry/src/main/java/advRocketry/Oxygen/OxygenSupplier.java
@@ -152,7 +152,6 @@ public class OxygenSupplier {
     }
 
     // get my current remaining scan limit
-    // TODO: this algorithm should support individual max scan size, so allow each block to have its own block scan limit
     private int getRemainingScanLimit() {
         return getBlockLimit() - scannedBlocksCounter;
     }
