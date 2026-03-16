af4ac3056d600c9b6e802d0108c06f277ea88d92
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-03-16 01:12:38 +0100

    Update API.java

--- advRocketry/src/main/java/API.java
+++ advRocketry/src/main/java/API.java
@@ -13,16 +13,16 @@ public class API {
         return (getDimension(levelId, isClientSide) instanceof Dimension dimension && dimension.hasEnoughOxygenToBurn());
     }
 
-    public static int getAvailableGasMB(ResourceLocation levelId, String gasId, boolean isClientside) {
+    public static double getAvailableGasInBuckets(ResourceLocation levelId, String gasId, boolean isClientside) {
         if (getDimension(levelId, isClientside) instanceof PlanetDimension planet) {
-            return (int) (planet.getGasProperty(gasId).in_atm / Config.INSTANCE.fluid_Contribution_To_Composition_Per_1000MB * 1000);
+            return planet.getGasProperty(gasId).in_atm * planet.getGravitationalMultiplier() / Config.INSTANCE.fluid_Contribution_To_Composition_Per_1000MB;
         }
         return 0;
     }
 
-    public static void addGasMB(ResourceLocation levelId, String gasId, int mb, boolean isClientside) {
+    public static void addGasMB(ResourceLocation levelId, String gasId, double bucket, boolean isClientside) {
         if (getDimension(levelId, isClientside) instanceof PlanetDimension planet) {
-            double toAdd = (double) mb / 1000 * Config.INSTANCE.fluid_Contribution_To_Composition_Per_1000MB;
+            double toAdd = bucket * Config.INSTANCE.fluid_Contribution_To_Composition_Per_1000MB / planet.getGravitationalMultiplier();
             planet.getGasProperty(gasId).in_atm += toAdd;
         }
     }
