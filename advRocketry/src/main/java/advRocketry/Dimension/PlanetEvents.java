48619975b1343af4541bdb0226d4945fe7d0f429
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-03-16 21:23:23 +0100

    Update PlanetEvents.java

--- advRocketry/src/main/java/advRocketry/Dimension/PlanetEvents.java
+++ advRocketry/src/main/java/advRocketry/Dimension/PlanetEvents.java
@@ -95,14 +95,14 @@ public class PlanetEvents {
         // to prevent taking all co2 from the atmosphere and causing a freeze
         PlanetDimensionProperties.GasProperty co2 = planet.getGasProperty(GasRegistry.co2);
         double oceanFractionWater = planet.getOceanFraction(GasRegistry.water);
-        if (oceanFractionWater > 0.1 && planet.getGasProperty(GasRegistry.water).liquid > 0 && co2.in_atm > 0) {
+        if (oceanFractionWater > 0.1 && planet.getGasProperty(GasRegistry.water).liquid > 0 && co2.in_atm > 0.0001001) {
             double sweetSpotForAlgae = 273.15 + 30;
             double maxTemperatureDeviationForAlgae = 15;
             double photosynthesisValue = 1 - Math.abs(sweetSpotForAlgae - planet.getCurrentTemp()) / maxTemperatureDeviationForAlgae;
             if (photosynthesisValue > 0) {
                 PlanetDimensionProperties.GasProperty o2 = planet.getGasProperty(GasRegistry.oxygen);
                 double toReduce = photosynthesisValue * Config.INSTANCE.planet_Photosynthesis_Factor;
-                toReduce = Math.min(toReduce, co2.in_atm);
+                toReduce = Math.min(toReduce, co2.in_atm - 0.0001);
                 if (!simulate) {
                     co2.in_atm -= toReduce;
                     o2.in_atm += toReduce;
