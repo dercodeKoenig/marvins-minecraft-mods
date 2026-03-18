65297b77851cfc55d2edbf593cfc2ea34364df6d
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-03-18 23:01:30 +0100

    .

--- advRocketry/src/main/java/advRocketry/BlockEntities/EntityFluidRelease.java
+++ advRocketry/src/main/java/advRocketry/BlockEntities/EntityFluidRelease.java
@@ -3,17 +3,36 @@ package advRocketry.BlockEntities;
 import ARLib.gui.GuiHandlerBlockEntity;
 import ARLib.gui.modules.guiModuleItemHandlerSlot;
 import ARLib.gui.modules.guiModulePlayerInventorySlot;
+import ARLib.network.PacketBlockEntity;
+import advRocketry.API;
+import advRocketry.Dimension.DimensionManager;
+import advRocketry.Dimension.PlanetDimension;
+import advRocketry.GlobalTime;
+import advRocketry.Registry.GasRegistry;
+import advRocketry.Render.Particles.RocketParticle;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.Vec3;
+import net.neoforged.neoforge.fluids.FluidStack;
 import net.neoforged.neoforge.fluids.capability.templates.FluidTank;
 import net.neoforged.neoforge.items.ItemStackHandler;
+import net.neoforged.neoforge.network.PacketDistributor;
+import org.joml.Vector3f;
 
 import static advRocketry.Registry.BlockEntities.ENTITY_CARGO_HOLD;
 import static advRocketry.Registry.BlockEntities.ENTITY_FLUID_RELEASE;
@@ -22,14 +41,40 @@ public class EntityFluidRelease extends BlockEntity implements ARLib.network.INe
 
     public FluidTank tank;
 
+    // Accumulate 1 bucket of water before placing the source for visualization.
+    // The placed / later removed water should not contribute to composition,
+    // but the player could still "farm" water by releasing 1 bucket, let it place the source, and take the source away.
+    // By accumulating 1 water before placing the source it makes sure player can not farm water
+    int accumulatedWaterBeforePlace = 0;
+    Fluid lastReleasedFluid = Fluids.EMPTY;
+
+    // if working, notify the clients every few ticks so they spawn particles
+    long lastParticleSent = 0;
+    // client will keep spawning particles after receiving the packet until timeout is reached
+    int clientParticleTimeout;
+
+
     public EntityFluidRelease(BlockPos pos, BlockState blockState) {
         super(ENTITY_FLUID_RELEASE.get(), pos, blockState);
-        tank = new FluidTank(10000){
+        tank = new FluidTank(10000) {
             @Override
-            public void onContentsChanged(){
+            public void onContentsChanged() {
                 setChanged();
             }
+
+            @Override
+            public boolean isFluidValid(FluidStack stack) {
+                for (GasRegistry.Gas gas : GasRegistry.gases.values()) {
+                    if (gas.fluid.equals(stack.getFluid()))
+                        return true;
+                }
+                return false;
+            }
         };
+    }
+
+    public static <T extends BlockEntity> void tick(Level level, BlockPos blockPos, BlockState blockState, T t) {
+        ((EntityFluidRelease) t).tick();
     }
 
     @Override
@@ -38,24 +83,103 @@ public class EntityFluidRelease extends BlockEntity implements ARLib.network.INe
 
     @Override
     public void readClient(CompoundTag compoundTag) {
+        if (compoundTag.contains("release_gas")) {
+            // reset timeout
+            clientParticleTimeout = 20;
+        }
     }
 
     @Override
     public void saveAdditional(CompoundTag tag, HolderLookup.Provider registries) {
         super.saveAdditional(tag, registries);
+        CompoundTag tankNbt = new CompoundTag();
+        tank.writeToNBT(registries, tankNbt);
+        tag.put("tank", tankNbt);
     }
 
     @Override
     public void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
         super.loadAdditional(tag, registries);
+        tank.readFromNBT(registries, tag.getCompound("tank"));
     }
 
     public void tick() {
 
-    }
+        if (level.isClientSide) {
+            if (clientParticleTimeout > 0) {
+                clientParticleTimeout--;
+                // reuse rocket particles
+                Vec3 worldPos = getBlockPos().getCenter();
+                Direction facing = getBlockState().getValue(BlockStateProperties.HORIZONTAL_FACING);
+                worldPos = worldPos.add(facing.getStepX() * 0.7, 0, facing.getStepZ() * 0.7);
+                new RocketParticle(
+                        (ClientLevel) level,
+                        worldPos.x + (Math.random() - 0.5) * 0.5,
+                        worldPos.y + (Math.random() - 0.5) * 0.5,
+                        worldPos.z + (Math.random() - 0.5) * 0.5,
+                        facing.getStepX() * 0.1,
+                        Math.random() * 0.1,
+                        facing.getStepZ() * 0.1,
+                        new Vector3f(0.5f, 0.5f, 0.5f).mul(1f),
+                        0.2f,
+                        1,
+                        100,
+                        false
+                );
+            }
+        }
 
-    public static <T extends BlockEntity> void tick(Level level, BlockPos blockPos, BlockState blockState, T t) {
-        ((EntityFluidRelease) t).tick();
-    }
+        if (!level.isClientSide) {
 
+            Direction facing = getBlockState().getValue(BlockStateProperties.HORIZONTAL_FACING);
+            BlockPos inFrontPos = getBlockPos().relative(facing);
+            BlockState inFrontState = level.getBlockState(inFrontPos);
+
+            // release all fluid into composition
+            FluidStack fluidStack = tank.getFluid();
+            if (!fluidStack.isEmpty() && inFrontState.canBeReplaced()) {
+                if (DimensionManager.INSTANCE_SERVER.get(level.dimension().location()) instanceof PlanetDimension planet) {
+                    for (GasRegistry.Gas gas : GasRegistry.gases.values()) {
+                        if (gas.fluid.equals(fluidStack.getFluid())) {
+                            // release all in atmosphere, the system will make sure it rains down if required
+                            API.addGasInBuckets(level.dimension().location(), gas.id, (double) fluidStack.getAmount() / 1000);
+                            break;
+                        }
+                    }
+                }
+                tank.setFluid(FluidStack.EMPTY);
+            }
+
+            // send particles over every few ticks
+            if (!lastReleasedFluid.equals(Fluids.EMPTY)) {
+                if (GlobalTime.getGlobalTime() > lastParticleSent + 20) {
+                    CompoundTag info = new CompoundTag();
+                    info.putInt("release_gas", 0);
+                    PacketDistributor.sendToPlayersTrackingChunk((ServerLevel) level, new ChunkPos(getBlockPos()), PacketBlockEntity.getBlockEntityPacket(this, info));
+                    lastParticleSent = GlobalTime.getGlobalTime();
+                }
+            }
+
+            // water special, place fluid (composition tracker ignores this class)
+            if (fluidStack.getFluid().equals(Fluids.WATER)) {
+                if (!inFrontState.getBlock().equals(Blocks.WATER)) {
+                    if (accumulatedWaterBeforePlace > 1000) {
+                        level.setBlock(inFrontPos, Blocks.WATER.defaultBlockState(), 3);
+                        accumulatedWaterBeforePlace = 0;
+                    } else {
+                        accumulatedWaterBeforePlace += fluidStack.getAmount();
+                        System.out.println(accumulatedWaterBeforePlace);
+                    }
+                }
+            }
+            // remove water in front of me again (composition tracker ignores this class)
+            else if (lastReleasedFluid.equals(Fluids.WATER)) {
+                if (inFrontState.getBlock().equals(Blocks.WATER) && inFrontState.getFluidState().isSource()) {
+                    level.setBlock(inFrontPos, Blocks.AIR.defaultBlockState(), 3);
+                }
+            }
+
+            lastReleasedFluid = fluidStack.getFluid();
+        }
+    }
 }
