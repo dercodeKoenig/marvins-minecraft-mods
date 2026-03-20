a3c1d06ae48e3257c5c958ba13a8b18a61a46196
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-03-20 23:42:29 +0100

    Update Config.java

--- advRocketry/src/main/java/advRocketry/Config.java
+++ advRocketry/src/main/java/advRocketry/Config.java
@@ -54,10 +54,10 @@ public class Config {
 
 
     // how much adding / removing 1000mb(1 bucket) of liquid should impact atmosphere composition
-    // default: 50000 buckets of fluid modify the composition by 1% of earth atmosphere
-    //          so you would require to remove 100 * 50k buckets to fully drain a gas
+    // default: 20000 buckets of fluid modify the composition by 1% of earth atmosphere
+    //          so you would require to remove 100 * 20k buckets to fully drain a gas
     //          if it has a presence of 1 in atmosphere
-    public double fluid_Contribution_To_Composition_Per_1000MB = 0.01 / 50000;
+    public double fluid_Contribution_To_Composition_Per_1000MB = 0.01 / 20000;
 
     // same as fluid contribution, but i give blocks more weight
     public double solid_Contribution_To_Composition_Per_Block = 0.01 / 2000;
