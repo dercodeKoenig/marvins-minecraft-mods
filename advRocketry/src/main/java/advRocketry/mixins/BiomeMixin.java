d4fddef438ab88ef51d34296900b634d168b464f
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-03-20 16:51:32 +0100

    Update BiomeMixin.java

--- advRocketry/src/main/java/advRocketry/mixins/BiomeMixin.java
+++ advRocketry/src/main/java/advRocketry/mixins/BiomeMixin.java
@@ -3,6 +3,7 @@ package advRocketry.mixins;
 import advRocketry.Dimension.Dimension;
 import advRocketry.Dimension.DimensionManager;
 import advRocketry.Dimension.PlanetDimension;
+import advRocketry.Dimension.SpaceStationDimension;
 import advRocketry.LifeSupport.LifeSupportSystem;
 import advRocketry.Registry.GasRegistry;
 import net.minecraft.core.BlockPos;
@@ -47,12 +48,26 @@ public abstract class BiomeMixin {
                 temp = 300;
             if(LifeSupportSystem.isPressureRegulated(serverLevel,water))
                 pressure = 1;
-            
+
+            if(dimension instanceof SpaceStationDimension){
+                // special case space stations:
+                // i want it to freeze when not regulated and not evaporate
+                // (maybe dimension event catches the block first and evaporates it, so there is a chance that evaporate or freeze can happen
+                if(!LifeSupportSystem.isPressureRegulated(serverLevel,water) &&
+                        LifeSupportSystem.isTemperatureRegulated(serverLevel,water))
+                {
+                    ci.setReturnValue(true);
+                    ci.cancel();
+                    return;
+                }
+            }
+
             GasRegistry.Gas waterGas = GasRegistry.gases.get(GasRegistry.water);
             if (temp > waterGas.getBoilingTemp(pressure)) {
                 // too hot for any ice, even in frozen biomes
                 ci.setReturnValue(false);
                 ci.cancel();
+                return;
             } else if (temp < waterGas.getFreezeTemp(pressure)) {
                 // cold enough to force freeze
                 // the default code of Biome class follows:
@@ -63,12 +78,14 @@ public abstract class BiomeMixin {
                         if (!mustBeAtEdge) {
                             ci.setReturnValue(true);
                             ci.cancel();
+                            return;
                         }
 
                         boolean flag = level.isWaterAt(water.west()) && level.isWaterAt(water.east()) && level.isWaterAt(water.north()) && level.isWaterAt(water.south());
                         if (!flag) {
                             ci.setReturnValue(true);
                             ci.cancel();
+                            return;
                         }
                     }
                 }
