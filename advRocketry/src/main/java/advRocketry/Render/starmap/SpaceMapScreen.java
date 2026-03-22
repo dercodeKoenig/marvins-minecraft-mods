65349807945aa35ca605d7ba04b1eab30f0035dd
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-03-22 20:11:43 +0100

    Update SpaceMapScreen.java

--- advRocketry/src/main/java/advRocketry/Render/starmap/SpaceMapScreen.java
+++ advRocketry/src/main/java/advRocketry/Render/starmap/SpaceMapScreen.java
@@ -463,6 +463,7 @@ public class SpaceMapScreen extends Screen {
         shader.getUniform("WorldMat").set(new Matrix4f());
         shader.getUniform("ModelMat").set(new Matrix4f());
         shader.getUniform("ProjMat").set(new Matrix4f().setPerspective(90F, (float) (windowWidth / windowHeight), 10F, 1000000F));
+        shader.getUniform("BrightnessModifier").set(1);
         shader.apply();
         SkyRenderer.vertexBufferStarBackground.bind();
         SkyRenderer.vertexBufferStarBackground.draw();
