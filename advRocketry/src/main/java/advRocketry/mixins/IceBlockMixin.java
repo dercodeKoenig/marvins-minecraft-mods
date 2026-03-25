0c2221f1555258292e54494b487104ca971b401c
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-03-25 23:32:01 +0100

    fix ice block mixin

--- advRocketry/src/main/java/advRocketry/mixins/IceBlockMixin.java
+++ advRocketry/src/main/java/advRocketry/mixins/IceBlockMixin.java
@@ -3,12 +3,15 @@ package advRocketry.mixins;
 import advRocketry.Dimension.Dimension;
 import advRocketry.Dimension.DimensionManager;
 import advRocketry.Dimension.PlanetDimension;
+import advRocketry.Dimension.SpaceStationDimension;
+import advRocketry.LifeSupport.LifeSupportSystem;
 import advRocketry.Registry.GasRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.IceBlock;
@@ -18,6 +21,7 @@ import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -34,7 +38,25 @@ public abstract class IceBlockMixin {
             cancellable = true)
     protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random, CallbackInfo ci) {
         if (DimensionManager.INSTANCE_SERVER.get(level.dimension().location()) instanceof PlanetDimension planet) {
-            if (!level.getBiome(pos).value().shouldFreeze(level, pos,false)) {
+
+            double temp = planet.getCurrentTemp();
+            double pressure = planet.getAtmosphereDensity();
+            if (LifeSupportSystem.isTemperatureRegulated(level, pos))
+                temp = 300;
+            if (LifeSupportSystem.isAirSupplyRegulated(level, pos))
+                pressure = Math.max(pressure, 1);
+
+
+            GasRegistry.Gas waterGas = GasRegistry.gases.get(GasRegistry.water);
+
+            if (temp < waterGas.getFreezeTemp(pressure)) {
+                // force freeze in any conditions, it is too cold
+                ci.cancel();
+                return;
+            }
+
+            if (temp > waterGas.getBoilingTemp(pressure)) {
+                // too hot for any ice
                 if (pos.getY() > planet.getGasProperty(GasRegistry.water).worldGenSeaLevel)
                     // melt into air because it is above sea level
                     // if it would melt into water, the position was probably already worked by the sea level adjustment
@@ -44,10 +66,16 @@ public abstract class IceBlockMixin {
                     level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
                 else
                     // normal melt into water
-                    // freeze and melt go from ice->water or water->ice so the composition tracker ignores it
+                    // freeze and melt go from ice->water or water->ice so the composition tracker ignores it,
+                    // (this class is also specifically excluded from composition change)
                     this.melt(state, level, pos);
+
+                ci.cancel();
+                return;
             }
-            ci.cancel();
+
+            // if not boiling and not freezing, let default logic run
+            // composition tracker still ignores melt from ice block class
         }
     }
 }
