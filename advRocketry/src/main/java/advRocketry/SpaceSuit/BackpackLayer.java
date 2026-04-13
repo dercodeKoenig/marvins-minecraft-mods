160fa6adfbaf5428f0106e295815258498f1b20e
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-04-13 15:01:00 +0200

    render jetpack and tanks

--- advRocketry/src/main/java/advRocketry/SpaceSuit/BackpackLayer.java
+++ advRocketry/src/main/java/advRocketry/SpaceSuit/BackpackLayer.java
@@ -13,6 +13,7 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
@@ -23,6 +24,7 @@ public class BackpackLayer<T extends LivingEntity, M extends PlayerModel<T>> ext
 
     static ResourceLocation texture = ResourceLocation.fromNamespaceAndPath(Main.MODID, "textures/models/armor/jetpack.png");
     static WavefrontObject model;
+
     static {
         try {
             model = new WavefrontObject(ResourceLocation.fromNamespaceAndPath(Main.MODID, "models/armor/jetpack.obj"));
@@ -42,6 +44,9 @@ public class BackpackLayer<T extends LivingEntity, M extends PlayerModel<T>> ext
         // Check if the player is wearing your specific armor
         if (chestStack.getItem() == Items.ITEM_SPACE_SUIT_CHESTPLATE.get()) {
             poseStack.pushPose();
+            CompoundTag cachedData = SpaceSuit.getCachedData(chestStack, entity.registryAccess());
+            int pressureTanks = cachedData.contains("pressureTanks") ? cachedData.getInt("pressureTanks") : 0;
+            boolean jetpack = false;
 
             VertexConsumer v = buffer.getBuffer(Static.ENTITY_SOLID_TRIANGLES.apply(texture));
 
@@ -50,16 +55,35 @@ public class BackpackLayer<T extends LivingEntity, M extends PlayerModel<T>> ext
 
             // 2. Adjust position (backplates usually need a slight Z-offset to stay behind the armor)
             // armor renders upside down for some reason so rotate
-            poseStack.rotateAround(new Quaternionf().fromAxisAngleDeg(0,0,1,180),0,0,0);
-            poseStack.translate(0, -0.1, 0);
+            poseStack.rotateAround(new Quaternionf().fromAxisAngleDeg(0, 0, 1, 180), 0, 0, 0);
+            poseStack.translate(0, -0.2, 0);
 
-            // 3. Call your OBJ Loader here
-            model.renderPart("center",poseStack,v,packedLight, OverlayTexture.NO_OVERLAY,0xffffffff);
-            model.renderPart("connections",poseStack,v,packedLight, OverlayTexture.NO_OVERLAY,0xffffffff);
-            model.renderPart("oxygenTank",poseStack,v,packedLight, OverlayTexture.NO_OVERLAY,0xffffffff);
-            model.renderPart("engine1",poseStack,v,packedLight, OverlayTexture.NO_OVERLAY,0xffffffff);
-            model.renderPart("engine2",poseStack,v,packedLight, OverlayTexture.NO_OVERLAY,0xffffffff);
-
+            // 3. Call your OBJ render here
+            model.renderPart("center", poseStack, v, packedLight, OverlayTexture.NO_OVERLAY, 0xffffffff);
+            if (pressureTanks == 1) {
+                // render single tank in center
+                model.renderPart("oxygenTank", poseStack, v, packedLight, OverlayTexture.NO_OVERLAY, 0xffffffff);
+            }
+            if (pressureTanks == 2) {
+                // render 2 tanks with the connections
+                model.renderPart("connections", poseStack, v, packedLight, OverlayTexture.NO_OVERLAY, 0xffffffff);
+                poseStack.pushPose();
+                poseStack.translate(0.11, 0, 0);
+                model.renderPart("oxygenTank", poseStack, v, packedLight, OverlayTexture.NO_OVERLAY, 0xffffffff);
+                poseStack.translate(-0.22, 0, 0);
+                model.renderPart("oxygenTank", poseStack, v, packedLight, OverlayTexture.NO_OVERLAY, 0xffffffff);
+                poseStack.popPose();
+            }
+            if(jetpack) {
+                // render the 2 jetpack engines
+                model.renderPart("connections", poseStack, v, packedLight, OverlayTexture.NO_OVERLAY, 0xffffffff);
+                poseStack.pushPose();
+                poseStack.translate(0.2, 0, 0.02);
+                model.renderPart("engine", poseStack, v, packedLight, OverlayTexture.NO_OVERLAY, 0xffffffff);
+                poseStack.translate(-0.4, 0, 0);
+                model.renderPart("engine", poseStack, v, packedLight, OverlayTexture.NO_OVERLAY, 0xffffffff);
+                poseStack.popPose();
+            }
             poseStack.popPose();
         }
     }
