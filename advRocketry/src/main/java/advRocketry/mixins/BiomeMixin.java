2ce38bb90a8260d13d0b87ec41c7fcde07ac688a
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-08-19 19:24:03 +0200

    Update BiomeMixin.java

--- advRocketry/src/main/java/advRocketry/mixins/BiomeMixin.java
+++ advRocketry/src/main/java/advRocketry/mixins/BiomeMixin.java
@@ -25,21 +25,33 @@ public abstract class BiomeMixin {
             at = @At("HEAD"),
             cancellable = true)
     public void shouldFreeze(LevelReader level, BlockPos water, boolean mustBeAtEdge, CallbackInfoReturnable<Boolean> ci) {
+        BlockState blockstate = level.getBlockState(water);
+        FluidState fluidstate = level.getFluidState(water);
+        if (fluidstate.getType() != Fluids.WATER) {
+            ci.setReturnValue(false);
+            return;
+        }
+
+        if (!(blockstate.getBlock() instanceof LiquidBlock)) {
+            ci.setReturnValue(false);
+            return;
+        }
+
+
         if (level instanceof ServerLevel serverLevel && DimensionManager.INSTANCE_SERVER.get(serverLevel.dimension().location()) instanceof Dimension dimension) {
             double temp = dimension.getCurrentTemp();
             double pressure = dimension.getAtmosphereDensity();
-            if(LifeSupportSystem.isTemperatureRegulated(serverLevel,water))
+            if (LifeSupportSystem.isTemperatureRegulated(serverLevel, water))
                 temp = 300;
-            if(LifeSupportSystem.isPressurized(serverLevel,water))
+            if (LifeSupportSystem.isPressurized(serverLevel, water))
                 pressure = Math.max(pressure, 1);
 
-            if(dimension instanceof SpaceStationDimension){
+            if (dimension instanceof SpaceStationDimension) {
                 // special case space stations:
                 // i want it to freeze when not pressurized no matter the temp
                 // note: if temperature is regulated, the dimension events may evaporate ice into air
                 //       so in this case it is a bit random if water freezes / evaporates
-                if(!LifeSupportSystem.isPressurized(serverLevel,water))
-                {
+                if (!LifeSupportSystem.isPressurized(serverLevel, water)) {
                     ci.setReturnValue(true);
                     ci.cancel();
                     return;
@@ -50,27 +62,20 @@ public abstract class BiomeMixin {
             if (temp > waterGas.getBoilingTemp(pressure)) {
                 // too hot for any ice, even in frozen biomes
                 ci.setReturnValue(false);
-                ci.cancel();
                 return;
             } else if (temp < waterGas.getFreezeTemp(pressure)) {
                 // cold enough to force freeze
                 // the default code of Biome class follows:
                 if (water.getY() >= level.getMinBuildHeight() && water.getY() < level.getMaxBuildHeight()) {
-                    BlockState blockstate = level.getBlockState(water);
-                    FluidState fluidstate = level.getFluidState(water);
-                    if (fluidstate.getType() == Fluids.WATER && blockstate.getBlock() instanceof LiquidBlock) {
-                        if (!mustBeAtEdge) {
-                            ci.setReturnValue(true);
-                            ci.cancel();
-                            return;
-                        }
+                    if (!mustBeAtEdge) {
+                        ci.setReturnValue(true);
+                        return;
+                    }
 
-                        boolean flag = level.isWaterAt(water.west()) && level.isWaterAt(water.east()) && level.isWaterAt(water.north()) && level.isWaterAt(water.south());
-                        if (!flag) {
-                            ci.setReturnValue(true);
-                            ci.cancel();
-                            return;
-                        }
+                    boolean flag = level.isWaterAt(water.west()) && level.isWaterAt(water.east()) && level.isWaterAt(water.north()) && level.isWaterAt(water.south());
+                    if (!flag) {
+                        ci.setReturnValue(true);
+                        return;
                     }
                 }
             }
