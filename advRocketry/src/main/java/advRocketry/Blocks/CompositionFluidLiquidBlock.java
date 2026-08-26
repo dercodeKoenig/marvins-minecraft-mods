eff7b067695b94eef08c4ab74b91f889acfca595
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-08-26 15:08:40 +0200

    Update CompositionFluidLiquidBlock.java

--- advRocketry/src/main/java/advRocketry/Blocks/CompositionFluidLiquidBlock.java
+++ advRocketry/src/main/java/advRocketry/Blocks/CompositionFluidLiquidBlock.java
@@ -39,7 +39,7 @@ public class CompositionFluidLiquidBlock extends LiquidBlock {
 
     protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
         if (DimensionManager.INSTANCE_SERVER.get(level.dimension().location()) instanceof PlanetDimension planet) {
-            if (planet.getCurrentTemp() > 1 + GasRegistry.gases.get(gasId).getBoilingTemp(planet.getAtmosphereDensity())) {
+            if(planet.shouldBoilBlocks(gasId, pos)) {
                 // too hot, boils away
                 level.setBlock(pos, state.setValue(PREVENT_COMPOSITION_CHANGE_ON_BREAK, true), 3);
                 level.setBlock(pos, Blocks.AIR.defaultBlockState(), 3);
