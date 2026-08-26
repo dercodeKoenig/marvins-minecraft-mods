8c2994cf2a1c92cc9dc0b2035eba7eeef3175c67
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2026-08-26 15:09:11 +0200

    Update Dimension.java

--- advRocketry/src/main/java/advRocketry/Dimension/Dimension.java
+++ advRocketry/src/main/java/advRocketry/Dimension/Dimension.java
@@ -1,29 +1,26 @@
 package advRocketry.Dimension;
 
 import advRocketry.GlobalTime;
+import advRocketry.LifeSupport.LifeSupportSystem;
 import advRocketry.Main;
+import advRocketry.Registry.GasRegistry;
 import advRocketry.Utils.AxisDirections;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
-import net.minecraft.world.level.chunk.ChunkAccess;
-import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.phys.Vec3;
-import net.neoforged.neoforge.event.level.ChunkDataEvent;
 import org.joml.Vector3f;
 
+import javax.annotation.Nullable;
 import java.util.*;
 
 public abstract class Dimension {
-    protected DimensionProperties properties;
-
-    protected StarCache starCache; // holds current main stars
-
-    boolean isClientSide;
     public DimensionManager dimensionManager;
-
     public HashMap<Long, ChunkInfo> loadedChunks = new HashMap<>();
+    protected DimensionProperties properties;
+    protected StarCache starCache; // holds current main stars
+    boolean isClientSide;
 
     public Dimension(DimensionProperties properties, DimensionManager dimensionManager) {
         this.properties = properties;
@@ -43,10 +40,11 @@ public abstract class Dimension {
         return DimensionManager.getServerLevel(getDimensionId());
     }
 
-    public void registerLoadedChunk(ChunkPos pos){
+    public void registerLoadedChunk(ChunkPos pos) {
         loadedChunks.put(pos.toLong(), new ChunkInfo());
     }
-    public void removeLoadedChunk(ChunkPos pos){
+
+    public void removeLoadedChunk(ChunkPos pos) {
         loadedChunks.remove(pos.toLong());
     }
 
@@ -84,6 +82,8 @@ public abstract class Dimension {
 
     abstract public float getAtmosphereDensity();
 
+    abstract public double getCurrentTemp();
+
     abstract public float getRadiationIntensity();
 
     abstract public boolean hasCustomSky();
@@ -100,9 +100,7 @@ public abstract class Dimension {
 
     abstract public Vec3 getMovement();
 
-    /**
-     * calculates universe space coordinates for the local font up coordinates of the dimension
-     */
+    // calculates universe space coordinates for the local font up coordinates of the dimension
     abstract public AxisDirections getGlobalAxisDirections(float partialTick);
 
     public void tick() {
@@ -110,7 +108,7 @@ public abstract class Dimension {
 
         if (!isClientSide) {
             int ticked = 0;
-            long t0 =System.nanoTime();
+            long t0 = System.nanoTime();
             ServerLevel level = level();
             for (Long i : loadedChunks.keySet()) {
                 ChunkPos pos = new ChunkPos(i);
@@ -119,35 +117,53 @@ public abstract class Dimension {
                     ticked++;
                 }
             }
-            long t1 =System.nanoTime();
-            /*
+            long t1 = System.nanoTime();
+            ///*
             if (GlobalTime.getGlobalTime() % 20 == 0 && loadedChunks.size() > 0) {
                 System.out.println("loaded chunks: " + loadedChunks.size());
                 System.out.println("ticked chunks: " + ticked);
-                System.out.println("time: " + (double)(t1-t0) / 1000 / 1000);
+                System.out.println("time: " + (double) (t1 - t0) / 1000 / 1000);
             }
-             */
+            //*/
         }
     }
 
-    public void tickChunk(ChunkPos pos){
+    public void tickChunk(ChunkPos pos) {
         ChunkInfo info = loadedChunks.get(pos.toLong());
         double p = 0.1;
-        if(info.isHotTimeout > 0) {
+        if (info.isHotTimeout > 0) {
             info.isHotTimeout--;
             p = 1;
         }
-        if(Math.random() < p) {
+        if (Math.random() < p) {
             if (DimensionEvents.performRandomTickEvents(this, level(), pos)) {
                 info.isHotTimeout = 200;
             }
         }
     }
 
-    abstract public double getCurrentTemp();
-
     public Iterable<ResourceLocation> getCurrentMainStars() {
         return starCache.significantLightSourcesCache.keySet();
+    }
+
+    public boolean shouldFreezeBlocks(String gasId, @Nullable BlockPos pos) {
+        double temp = getCurrentTemp();
+        double pressure = getAtmosphereDensity();
+        if (pos != null && LifeSupportSystem.isTemperatureRegulated(level(), pos))
+            temp = 300;
+        if (pos != null && LifeSupportSystem.isPressurized(level(), pos))
+            pressure = Math.max(pressure, 1);
+        return temp < GasRegistry.gases.get(gasId).getFreezeTemp(pressure);
+    }
+
+    public boolean shouldBoilBlocks(String gasId, @Nullable BlockPos pos) {
+        double temp = getCurrentTemp();
+        double pressure = getAtmosphereDensity();
+        if (pos != null && LifeSupportSystem.isTemperatureRegulated(level(), pos))
+            temp = 300;
+        if (pos != null && LifeSupportSystem.isPressurized(level(), pos))
+            pressure = Math.max(pressure, 1);
+        return temp > GasRegistry.gases.get(gasId).getBoilingTemp(pressure);
     }
 
     protected void tickStarCache() {
@@ -176,7 +192,7 @@ public abstract class Dimension {
         }
     }
 
-    public static class ChunkInfo{
+    public static class ChunkInfo {
         // timout for running with increased tick frequency when something interesting happens
         // while > 0: do more updates, while 0: do less updates
         int isHotTimeout = 0;
